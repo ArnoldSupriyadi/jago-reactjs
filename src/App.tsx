353cd98b87ac2e567fc5294e650b83529f29373a
ArnoldSupriyadi <arnoldsupriyadi@gmail.com>
2025-02-03 10:34:17 +0700

    Baru sampai materi REACT COMPONENT

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from './component/button/Button';
+import Avatar from './component/avatar/Avatar';
+import Input from './input/Input';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button />
+      <Avatar />
+      <Input />
     </div>
   );
 }
---
+++ src/input/Input.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Input = () => {
+    return (
+        <>
+            <input placeholder="Masukan data" />
+        </>
+    )
+};
+
+export default Input;
